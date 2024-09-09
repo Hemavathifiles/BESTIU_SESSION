9c2b7eded45caa9d8cb40aeeb3359546407d69c5
hemav <hemav@LAPTOP-BH7SCHNS>
2024-09-09 21:32:54 +0530

    Second commit

--- BestIU_Files/src/practice/HelloWorld.java
+++ BestIU_Files/src/practice/HelloWorld.java
@@ -3,7 +3,8 @@ package practice;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello students");
+		System.out.println("hello students to C2TC");
+	
 
 	}
 
