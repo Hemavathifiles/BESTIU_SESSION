18e6f1c9de50124f57ac44322ad46d59cfd2b61d
hemav <hemav@LAPTOP-BH7SCHNS>
2024-09-10 18:31:07 +0530

    Third commit

---
+++ BestIU_Files/src/practice/StringDemo.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String str="hello students welcome to the session";
+		System.out.println(str);
+
+	}
+
+}
