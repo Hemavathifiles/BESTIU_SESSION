e65eaa4987abd4603c0a279b517626b874a3679c
hemav <hemav@LAPTOP-BH7SCHNS>
2024-09-11 12:51:28 +0530

    Fourth commit

---
+++ BestIU_Files/src/practice/ForEachDemo.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		int num[]= {2,3,4,5};
+		for (int print:num)
+		{
+			System.out.println(print);
+		}
+
+	}
+
+}
