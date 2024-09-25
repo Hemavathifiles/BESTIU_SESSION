5ca260e82e9bb6c82908899dd0a1f455b44321e5
hemav <hemav@LAPTOP-BH7SCHNS>
2024-09-25 19:02:21 +0530

    eleventh commit

---
+++ BestIU_Files/src/org/tns/capgemini/c2tc/Scannerclass_BufferReader/BufferReaderDemo.java
@@ -0,0 +1,16 @@
+package org.tns.capgemini.c2tc.Scannerclass_BufferReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class BufferReaderDemo {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new FileReader("C:\\Users\\hemav\\OneDrive\\Desktop\\Java Notes\\demo.txt"));
+		String data="";
+		while((data=br.readLine())!=null) 
+			{
+			System.out.println(data);
+			}
+		br.close();
+}
+}
