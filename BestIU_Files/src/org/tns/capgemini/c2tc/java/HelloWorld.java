af1e02a4ca877a2409c62c3407c18d165ffc61e6
hemav <hemav@LAPTOP-BH7SCHNS>
2024-10-17 21:07:59 +0530

    19 commit

--- BestIU_Files/src/org/tns/capgemini/c2tc/java/HelloWorld.java
+++ BestIU_Files/src/org/tns/capgemini/c2tc/java/HelloWorld.java
@@ -3,7 +3,7 @@ package org.tns.capgemini.c2tc.java;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello students to C2TC");
+		System.out.println("hello students ");
 	
 
 	}
